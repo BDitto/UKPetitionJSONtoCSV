a132c9c10451d9f80fb95ef4c89676a38540497d
Si <54117024+BDitto@users.noreply.github.com>
2022-04-27 18:33:25 +0100

    Rewrite: Function Call, Verbose Logging etc.
    Rewrite of the Script to improve import capability to other projects. 
    
    Script now made into a Function call "UKPetitionJSONtoCSV"
    Verbose Print statements now made optional to allow for ""quiet"" deployment within other projects
    Enabled options for output when automating
    
    Script now considered "complete" future changes are likely to be minor

--- UKPetitionConversion.py
+++ UKPetitionConversion.py
@@ -1,69 +1,109 @@
 import pandas as pd, urllib.request as web, json
 
-#script information
-print("\n --------------------------------- \n "
-      "UK Parlimnet JSON to CSV converter v0.220422" 
-      "\n --------------------------------- \n"
-      " This script is provided \"AS IS\" for the purposes of converting UK Parlimentary Petition data to a CSV format readable in most data packages (Excel, SAS, Tableu, Google Data Studio etc). This file will create 3 csv files in directory:\n"
-      "\n Signatures by Country\n"
-      "\n Signatures by Region\n"
-      "\n Signatures by Constituency\n"
-      "\n Field titles shall be saved as presentend by the UK Parlimentary Petitions data source"
-      "\n \n No garuntees are expressed or implied by the author in making this script available. \n \n This script is made available under Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) License \n Please remember to credit this script in your final outputs also\n --------------------------------- \n")
+# ---------------------------------
+#UK Parliment JSON to CSV converter
+version = "v1.220427"
+#Last Updated: 27 April 2022
+#---------------------------------
+#This script is provided AS IS for the purposes of converting UK Parlimentary Petition data to a CSV format readable in most data packages (Excel, SAS, Tableu, Google Data Studio etc). This file will create 3 csv files in directory:
+#
+#       Signatures by Country
+#       Signatures by Region
+#       Signatures by Constituency
+#
+# Field titles shall be saved as presentend by the UK Parlimentary Petitions data source
+#
+# No garuntees are expressed or implied by the author in making this script available.
+# This script is made available under Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) license
+#
+# Please remember to credit this script in your final outputs also
+#
+# Please see README at github.com/BDitto/UKPetitionJSONtoCSV
+# ---------------------------------
+
+def UKPetitionJSONtoCSV(automate = 0,pettitionID = 0, outputName = "", CountryOutput = 1, RegionOutput = 1, constituencyOutput = 1, verbose = 1):
+
+    #set verbose terminal logging:
+
+    if automate == 0:
+        #verbose logging is always enabled where automation is not utalised in order to ensure clear script progression feedback
+        verbose = 1
+
+    if verbose == 1:
+        print("\n Verbose logging enabled: UK Parlimnet JSON to CSV converter " + version + "\n")
 
 
-#retrieve petition
+    #retrieve petition from user manually
 
-retloop = 0
-while retloop != "1":
-      print("Please enter/paste the pettition ID for the pettition you wish to convert (ie the last part of the pettition URL)")
-      petURL = input("ID = ")
-      print("\n --------------------------------- \n"
-            "retrieving"
-            "\n --------------------------------- \n")
+    if automate == 0:
+        retloop = 0
+        while retloop != "1":
+            print("Please enter/paste the pettition ID for the pettition you wish to convert (ie the last part of the pettition URL)")
+            petURL = input("ID = ")
+            print("\n --------------------------------- \n"
+                  "retrieving"
+                  "\n --------------------------------- \n")
 
-      #load data
-      js = json.load(web.urlopen("https://petition.parliament.uk/petitions/"+ str(petURL) +".json"))
-      #print(js["data"]["attributes"].keys())
-
-
-      print("this petition ID return the title:\n"
-            "\"" +js["data"]["attributes"]["action"]+"\" \n")
-
-      answer = "begin"
-      while answer != "1" and answer != "0":
-            print("Is this correct? \n Yes [1] \n no [0] \n")
-            answer = str(input(""))
-            print(answer)
-
-      retloop = answer
-
-#request output name:
-print("Enter Output name: \n (This will be used to label output files)")
-PetitionID = input("")
-
-#load keys to verify file and simplyfy coding later as a reference point for keys
-#print(js["data"]["attributes"].keys())
+            #load data
+            js = json.load(web.urlopen("https://petition.parliament.uk/petitions/"+ str(petURL) +".json"))
 
 
-#convert JSON to Data Frame
-countryDF = pd.DataFrame(js["data"]["attributes"]["signatures_by_country"])
-constituency =  pd.DataFrame(js["data"]["attributes"]["signatures_by_constituency"])
-region =  pd.DataFrame(js["data"]["attributes"]["signatures_by_region"])
+            print("this petition ID return the title:\n"
+                  "\"" +js["data"]["attributes"]["action"]+"\" \n")
 
+            answer = "begin"
+            while answer != "1" and answer != "0":
+                print("Is this correct? \n Yes [1] \n no [0] \n")
+                answer = str(input(""))
+                print(answer)
 
-#Optional choice to add UK apprend to consituency for google data studio inport
-#constituency['name'] = constituency['name'] + ", UK"
+            retloop = answer
 
-#save country dataframes
-countryDF.to_csv(PetitionID + "_By_Country.csv", index=False)
-constituency.to_csv(PetitionID + "_By_constituency.csv", index=False)
-region.to_csv(PetitionID + "_by_region.csv", index=False)
+    #auto retrieve petition
+    if automate == 1:
+        if verbose == 1:
+            print("Loading Pettition")
+        js = json.load(web.urlopen("https://petition.parliament.uk/petitions/" + str(pettitionID) + ".json"))
+        if verbose == 1:
+            print("petition Title:\n"
+                  "\"" + js["data"]["attributes"]["action"] + "\" \n")
 
-print("\n --------------------------------- \n"
-      "individuals from " + str(len(constituency)) + " constituencys signed this peition. For binning purposes, there are " + str(len(constituency["signature_count"].unique())) + " unique values \n\n "
-      "individuals from " + str(len(region)) + " regions signed this peition. For binning purposes, there are " + str(len(region["signature_count"].unique())) + " unique values \n\n"
-      "individuals from " + str(len(countryDF)) + " countries signed this peition. For binning purposes, there are " + str(len(countryDF["signature_count"].unique())) + " unique values \n"
-      "\n --------------------------------- \n")
+    #request manual output name:
+    if automate == 0:
+        print("Enter Output name: \n (This will be used to label output files)")
+        outputName = input("")
 
-print(" \n\n --script complete--")
+    if automate == 1:
+        if verbose == 1:
+            print("\n output prefixed with " + outputName)
+
+    #convert JSON to Data Frames and export
+
+    if verbose ==1:
+        print("\n Begging Conversion and Saving Output \n")
+
+    if CountryOutput == 1:
+        countryDF = pd.DataFrame(js["data"]["attributes"]["signatures_by_country"])
+        countryDF.to_csv(str(outputName) + "_By_Country.csv", index=False)
+        if verbose == 1:
+            print("Country Conversion Complete")
+            print("individuals from " + str(
+                len(countryDF)) + " countries signed this peition. For binning purposes, there are " + str(
+                len(countryDF["signature_count"].unique())) + " unique values \n\n")
+
+    if RegionOutput == 1:
+        region = pd.DataFrame(js["data"]["attributes"]["signatures_by_region"])
+        region.to_csv(str(outputName) + "_by_region.csv", index=False)
+        if verbose == 1:
+            print("Region Conversion Complete")
+            print("individuals from " + str(len(region)) + " regions signed this peition. For binning purposes, there are " + str(len(region["signature_count"].unique())) + " unique values \n\n")
+
+    if constituencyOutput== 1:
+        constituency = pd.DataFrame(js["data"]["attributes"]["signatures_by_constituency"])
+        constituency.to_csv(str(outputName) + "_By_constituency.csv", index=False)
+        if verbose == 1:
+            print("Constituency Conversion Complete")
+            print("individuals from " + str(len(constituency)) + " constituencys signed this peition. For binning purposes, there are " + str(len(constituency["signature_count"].unique())) + " unique values \n\n ")
+
+    if verbose == 1:
+        print(" \n\n script complete")
